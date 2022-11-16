d0563fbb681fbec6e0c8ee705c320268f29d682a
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2022-11-15 21:41:00 -0500

    Initializes repo

---
+++ gworv/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)(019(m3*i(pt*2(&zsm=sh4#35=88nhz49cv*e22-$-di6j9y"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ involvement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InvolvementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'involvement'
